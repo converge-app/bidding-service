868f6fe0437f645460a1a376410571d8c4286ab9
samirhabibi <samir-habibi1@hotmail.com>
2019-11-24 16:19:15 +0100

    added tests

--- ApplicationUnitTests/BidServiceTest.cs
+++ ApplicationUnitTests/BidServiceTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
 using Application.Exceptions;
 using Application.Models.Entities;
 using Application.Repositories;
@@ -12,17 +16,46 @@ namespace ApplicationUnitTests
     public class BidServiceTest
     {
         [Fact]
+        public async void Create_GetUserAsync()
+        {
+            Environment.SetEnvironmentVariable("USERS_SERVICE_HTTP", "users-service.api.converge-app.net");
+            var expected = "";
+            var mockFactory = new Mock<IHttpClientFactory>();
+            var configuration = new HttpConfiguration();
+            var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+            {
+                request.SetConfiguration(configuration);
+                var response = request.CreateResponse(HttpStatusCode.OK, expected);
+                return Task.FromResult(response);
+            });
+
+            var client = new HttpClient(clientHandlerStub);
+
+            mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+            IHttpClientFactory factory = mockFactory.Object;
+            var controller = new Client(factory);
+
+            //Act
+            var result = await controller.GetUserAsync("123");
+
+            //Assert
+            Assert.NotNull(expected);
+        }
+
+        [Fact]
         public void Open_GetProject_ThrowsInvalidBid()
         {
             // Arrange
             var biddingRepository = new Mock<IBidRepository>();
             var client = new Mock<IClient>();
+            biddingRepository.Setup(m => m.Create(It.IsAny<Bid>())).ReturnsAsync((Bid)null);
             var biddingService = new BidService(biddingRepository.Object, client.Object);
 
             // Act
             // Assert
 
-            Assert.ThrowsAsync<InvalidBid>(() => biddingService.Open(new Bid() { ProjectId = null }));
+            Assert.ThrowsAsync<InvalidBid>(() => biddingService.Open(new Bid()));
 
         }
 
@@ -32,6 +65,7 @@ namespace ApplicationUnitTests
             // Arrange
             var biddingRepository = new Mock<IBidRepository>();
             var client = new Mock<IClient>();
+
             biddingRepository.Setup(m => m.Create(It.IsAny<Bid>())).ReturnsAsync(new Bid());
             var biddingService = new BidService(biddingRepository.Object, client.Object);
 
